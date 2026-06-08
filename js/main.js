021d9e294e5d8fc18c54d72a8384be577298df68
ahmadmashari720-cyber <ahmadmashari720@gmail.com>
2026-06-08 12:25:52 +0300

    Refactor furniture display with updated names
    Updated furniture item names and added headings to cards.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const furnitureItems = [
-    { id: 1, name: "طاولة بلوط عتيقة", image: "https://images.unsplash.com/photo-1533090161767-e6ffed986c88" },
+    { id: 1, name: "طاولة عتيقة", image: "https://images.unsplash.com/photo-1533090161767-e6ffed986c88" },
     { id: 2, name: "أريكة مودرن", image: "https://images.unsplash.com/photo-1555041469-a586c61ea9bc" },
     { id: 3, name: "كرسي ذراع أنتيك", image: "https://images.unsplash.com/photo-1586023492125-27b2c045efd7" },
     { id: 4, name: "خزانة كتب خشبية", image: "https://images.unsplash.com/photo-1594620302200-9a762244a156" },
@@ -22,6 +22,7 @@ function displayFurniture() {
         card.innerHTML = `
             <img src="${item.image}?auto=format&fit=crop&w=400&q=80" alt="${item.name}">
             <div class="card-content">
+                <h4>${item.name}</h4>
                 <a href="${whatsappLink}" target="_blank" class="whatsapp-btn">تواصل عبر واتساب</a>
             </div>
         `;
